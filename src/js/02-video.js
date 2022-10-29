294cdce464f4814a6d51021364a2ca4e0b3eca2d
Marina Zalozna <marina.zalozna@gmail.com>
2022-10-29 20:41:10 +0300

    4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+function whenOn(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+player.on('timeupdate', throttle(whenOn, 1000));
+
+function onPlay(data) {
+  let savedTime = localStorage.getItem('videoplayer-current-time');
+  if (data.seconds !== savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
+player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(input, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+fillInputs();
+
+function input() {
+  const data = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function fillInputs() {
+  const savedInfo = localStorage.getItem('feedback-form-state');
+
+  if (savedInfo) {
+    refs.input.value = JSON.parse(savedInfo).email;
+    refs.textarea.value = JSON.parse(savedInfo).message;
+  }
+}
